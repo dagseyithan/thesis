c4b00dea97657855b5d876dfd49151dfbfc3bd36
Seyithan Dag <dagseyithan@gmail.com>
2019-03-11 22:37:36 +0100

    first

--- elmo.py
+++ elmo.py
@@ -3,5 +3,5 @@ from elmoformanylangs import Embedder
 embedder = Embedder('C:\\Users\\seyit\\PycharmProjects\\thesis\\elmoformanylangs\\elmo_german_embeddings')
 
 
-def get_elmo_vector(text):
+def get_elmo_embedding(text):
     return embedder.sents2elmo([[text]])
