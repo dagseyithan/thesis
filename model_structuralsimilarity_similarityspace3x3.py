6246377c6f97bb8cb5afba9102bfe7ac42df8cef
Seyithan Dag <dagseyithan@gmail.com>
2019-07-26 02:25:15 +0200

    update

--- model_structuralsimilarity_similarityspace3x3.py
+++ model_structuralsimilarity_similarityspace3x3.py
@@ -3,7 +3,6 @@ from keras.models import Model, load_model
 from keras.optimizers import Adam, Adagrad
 from keras.callbacks import TensorBoard, LambdaCallback
 from data_utilities.generator import Native_DataGenerator_for_StructuralSimilarityModel_SimilaritySpace3x3
-from keras.callbacks import TensorBoard, LambdaCallback, ReduceLROnPlateau, EarlyStopping
 import time
 import keras.backend as K
 import tensorflow as tf
@@ -15,14 +14,12 @@ def create_network():
     input_a = Input(shape=(DIM,))
     input_b = Input(shape=(DIM,))
 
+
     def common_network():
-        layers = [Reshape((2, 2)),
-                  LSTM(units=200, activation='relu', return_sequences=True, input_shape=(2, 2)),
-                  Reshape((1, 20, 20)),
+        layers = [Reshape((1, 2, 2)),
                   Conv2D(filters=200, kernel_size=(2, 2), kernel_initializer='glorot_uniform',
-                        input_shape=(1, 20, 20), data_format='channels_first',
-                        use_bias=True, activation='relu', padding='same'),
-                  MaxPooling2D(pool_size=(10, 10), strides=None, padding='valid', data_format='channels_first'),
+                         input_shape=(1, 2, 2), data_format='channels_first',
+                         use_bias=True, activation='relu', padding='valid')
                   ]
 
         def shared_layers(x):
@@ -42,6 +39,7 @@ def create_network():
     x = concat_out
     x = Dense(activation='relu', units=200, use_bias=True)(x)
     x = Dense(activation='relu', units=100, use_bias=True)(x)
+    x = Dense(activation='relu', units=10, use_bias=True)(x)
     out = Dense(activation='relu', units=1, use_bias=True)(x)
 
     model = Model(inputs=[input_a, input_b], outputs=out)
@@ -60,8 +58,7 @@ tensorboard = TensorBoard(log_dir='./logs/model_structuralsimilarity/' + model_n
                           update_freq='epoch')
 
 
-reduce_lr_callback = ReduceLROnPlateau(monitor='loss', patience=10, factor=0.1, verbose=1, min_lr=0.0001)
-early_stopping_callback = EarlyStopping(monitor='loss', min_delta=0.0, patience=100)
-data_generator = Native_DataGenerator_for_StructuralSimilarityModel_SimilaritySpace3x3(batch_size=200)
-model.fit_generator(generator=data_generator, shuffle=True, epochs=300, workers=4, use_multiprocessing=False, callbacks=[tensorboard, reduce_lr_callback, early_stopping_callback])
+
+data_generator = Native_DataGenerator_for_StructuralSimilarityModel_SimilaritySpace3x3(batch_size=20)
+model.fit_generator(generator=data_generator, shuffle=True, epochs=300, workers=4, use_multiprocessing=False, callbacks=[tensorboard])
 model.save(filepath='trained_models/'+model_name+'.h5')
