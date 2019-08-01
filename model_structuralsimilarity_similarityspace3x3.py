ff8ed9d9a9398494bbeaa67961b8ba73435b3c21
Seyithan Dag <dagseyithan@gmail.com>
2019-08-01 12:26:08 +0200

    update

--- model_structuralsimilarity_similarityspace3x3.py
+++ model_structuralsimilarity_similarityspace3x3.py
@@ -1,7 +1,7 @@
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Input, concatenate, Bidirectional, LSTM, Reshape
 from keras.models import Model, load_model
 from keras.optimizers import Adam, Adagrad
-from keras.callbacks import TensorBoard, LambdaCallback
+from keras.callbacks import TensorBoard, LambdaCallback, ReduceLROnPlateau, EarlyStopping
 from data_utilities.generator import Native_DataGenerator_for_StructuralSimilarityModel_SimilaritySpace3x3
 import time
 import keras.backend as K
@@ -17,7 +17,7 @@ def create_network():
 
     def common_network():
         layers = [Reshape((1, 2, 2)),
-                  Conv2D(filters=200, kernel_size=(2, 2), kernel_initializer='glorot_uniform',
+                  Conv2D(filters=50, kernel_size=(2, 2), kernel_initializer='glorot_uniform',
                          input_shape=(1, 2, 2), data_format='channels_first',
                          use_bias=True, activation='relu', padding='valid')
                   ]
@@ -37,9 +37,10 @@ def create_network():
     concat_out = concatenate([out_a, out_b])
 
     x = concat_out
-    x = Dense(activation='relu', units=200, use_bias=True)(x)
     x = Dense(activation='relu', units=100, use_bias=True)(x)
-    x = Dense(activation='relu', units=10, use_bias=True)(x)
+    x = Dense(activation='relu', units=50, use_bias=True)(x)
+    x = Dense(activation='relu', units=25, use_bias=True)(x)
+    x = Dense(activation='relu', units=5, use_bias=True)(x)
     out = Dense(activation='relu', units=1, use_bias=True)(x)
 
     model = Model(inputs=[input_a, input_b], outputs=out)
@@ -52,13 +53,15 @@ model = create_network()
 model.summary()
 model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error')
 model_name = 'model_structuralsimilarity_similarityspace3x3'
-tensorboard = TensorBoard(log_dir='./logs/model_structuralsimilarity/' + model_name +time.strftime("%Y%m%d%H%M%S"), histogram_freq=0, batch_size=120,
+time_stamp = time.strftime("%Y%m%d%H%M%S")
+tensorboard = TensorBoard(log_dir='./logs/model_structuralsimilarity/' + model_name + time_stamp, histogram_freq=0,
                           write_graph=True, write_grads=False, write_images=False, embeddings_freq=0,
                           embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None,
                           update_freq='epoch')
 
-
+reduce_lr_callback = ReduceLROnPlateau(monitor='loss', patience=10, factor=0.1, verbose=1, min_lr=0.0001)
+early_stopping_callback = EarlyStopping(monitor='loss', min_delta=0.0, patience=100)
 
 data_generator = Native_DataGenerator_for_StructuralSimilarityModel_SimilaritySpace3x3(batch_size=20)
-model.fit_generator(generator=data_generator, shuffle=True, epochs=300, workers=4, use_multiprocessing=False, callbacks=[tensorboard])
-model.save(filepath='trained_models/'+model_name+'.h5')
+model.fit_generator(generator=data_generator, shuffle=True, epochs=500, workers=4, use_multiprocessing=False, callbacks=[tensorboard, reduce_lr_callback, early_stopping_callback])
+model.save(filepath='trained_models/'+model_name + time_stamp+'.h5')
