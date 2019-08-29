01574813d610909153d36d4c14f51b05a65ccd95
dagseyithan <dagseyithan@gmail.com>
2019-08-29 05:14:38 +0000

    update-cloud-westb

--- semantic_similarity_network_alt.py
+++ semantic_similarity_network_alt.py
@@ -37,10 +37,10 @@ class MLPConv(Layer):
 
     def build(self, input_shape):
         super(MLPConv, self).build(input_shape)
-        self.dense_1 = Dense(100, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_2 = Dense(50, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_3 = Dense(25, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_4 = Dense(1, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_1 = Dense(500, activation='relu', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_2 = Dense(250, activation='relu', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_3 = Dense(50, activation='relu', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_4 = Dense(1, activation='relu', kernel_initializer='glorot_uniform', use_bias=True)
 
     def call(self, input):
         x = self.dense_1(input)
@@ -60,16 +60,18 @@ class MLPLSTM(Layer):
 
     def build(self, input_shape):
         super(MLPLSTM, self).build(input_shape)
-        self.dense_1 = Dense(1000, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_2 = Dense(100, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_3 = Dense(10, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_4 = Dense(1, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_1 = Dense(6000, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_2 = Dense(600, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_3 = Dense(60, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_4 = Dense(6, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_5 = Dense(1, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
 
     def call(self, input):
         x = self.dense_1(input)
         x = self.dense_2(x)
         x = self.dense_3(x)
         x = self.dense_4(x)
+        x = self.dense_5(x)
         return x
 
     def compute_output_shape(self, input_shape):
@@ -83,8 +85,17 @@ class MLPScore(Layer):
 
     def build(self, input_shape):
         super(MLPScore, self).build(input_shape)
-        self.dense_1 = Dense(5, activation=selected_activation, kernel_initializer='glorot_uniform', use_bias=True)
-        self.dense_2 = Dense(1, activation='sigmoid', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_1 = Dense(3600, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_2 = Dense(360, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_3 = Dense(36, activation='tanh', kernel_initializer='glorot_uniform', use_bias=True)
+        self.dense_4 = Dense(1, activation='sigmoid', kernel_initializer='glorot_uniform', use_bias=True)
+
+    def call(self, input):
+        x = self.dense_1(input)
+        x = self.dense_2(x)
+        x = self.dense_3(x)
+        x = self.dense_4(x)
+        return x
 
     def call(self, input):
         x = self.dense_1(input)
@@ -97,23 +108,23 @@ class MLPScore(Layer):
 
 def SemanticSimilarityNetwork():
 
-    embedded_sentence_A = Input(shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH)) #(20, 300)
-    embedded_sentence_B = Input(shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH)) #(20, 300)
+    embedded_sentence_A = Input(shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH)) #(10, 300)
+    embedded_sentence_B = Input(shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH)) #(10, 300)
 
 
-    conv1D_2n = Conv1D(filters=10, kernel_size=2, kernel_initializer='glorot_uniform',
+    conv1D_2n = Conv1D(filters=40, kernel_size=2, kernel_initializer='glorot_uniform',
                        bias_initializer='glorot_uniform',input_shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH),
-                       use_bias=True, activation=selected_activation, padding='same', name='conv1D_2n')
-    conv1D_3n = Conv1D(filters=10, kernel_size=3, kernel_initializer='glorot_uniform',
+                       use_bias=True, activation='tanh', padding='same', name='conv1D_2n')
+    conv1D_3n = Conv1D(filters=40, kernel_size=3, kernel_initializer='glorot_uniform',
                        bias_initializer='glorot_uniform',input_shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH),
-                       use_bias=True, activation=selected_activation,padding='same', name='conv1D_3n')
-    conv2D_2x2 = Conv2D(filters=10, kernel_size=(2, 2), kernel_initializer='glorot_uniform',
+                       use_bias=True, activation='tanh',padding='same', name='conv1D_3n')
+    conv2D_2x2 = Conv2D(filters=40, kernel_size=(2, 2), kernel_initializer='glorot_uniform',
                         bias_initializer='glorot_uniform', input_shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH),
-                        use_bias=True, activation=selected_activation,
+                        use_bias=True, activation='tanh',
                         padding='same', name='conv2D_2x2')
-    conv2D_3x3 = Conv2D(filters=10, kernel_size=(3,3), kernel_initializer='glorot_uniform',
+    conv2D_3x3 = Conv2D(filters=40, kernel_size=(3,3), kernel_initializer='glorot_uniform',
                         bias_initializer='glorot_uniform', input_shape=(MAX_TEXT_WORD_LENGTH, EMBEDDING_LENGTH),
-                        use_bias=True, activation=selected_activation, padding='same', name='conv2D_3x3')
+                        use_bias=True, activation='tanh', padding='same', name='conv2D_3x3')
     maxPool2D_2x2 = MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format='channels_first',
                                  name='maxPool2D_2x2')
     averagePool2D_2x2 = AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format='channels_first',
@@ -156,35 +167,35 @@ def SemanticSimilarityNetwork():
     conv2D_3x3_out_B = conv2D_3x3(expand_dims(embedded_sentence_B))
     bi_lstms_out_B = bi_lstms(embedded_sentence_B)
 
-    concat_conv1D_2n = concatenate([conv1D_2n_out_A, conv1D_2n_out_B], axis=-1)
-    concat_conv1D_3n = concatenate([conv1D_3n_out_A, conv1D_3n_out_B], axis=-1)
-    concat_2D_2x2 = concatenate([conv2D_2x2_out_A,  conv2D_2x2_out_B], axis=-1)
-    concat_2D_3x3 = concatenate([conv2D_3x3_out_A, conv2D_3x3_out_B], axis=-1)
-    concat_bilstms = concatenate([bi_lstms_out_A, bi_lstms_out_B], axis=-1)
+    concat_conv1D_2n = concatenate([conv1D_2n_out_A, conv1D_2n_out_B], axis=1)
+    concat_conv1D_3n = concatenate([conv1D_3n_out_A, conv1D_3n_out_B], axis=1)
+    concat_2D_2x2 = concatenate([conv2D_2x2_out_A,  conv2D_2x2_out_B], axis=1)
+    concat_2D_3x3 = concatenate([conv2D_3x3_out_A, conv2D_3x3_out_B], axis=1)
+    concat_bilstms = concatenate([bi_lstms_out_A, bi_lstms_out_B], axis=1)
 
-    pooled_1D_2n = maxPool2D_2x2(expand_dims(concat_conv1D_2n))
-    pooled_1D_2n = Flatten()(pooled_1D_2n)
-    pooled_1D_2n = MLP_pooled_1D_2n(pooled_1D_2n)
+    pooled_1D_2n = Flatten()(maxPool2D_2x2(expand_dims(concat_conv1D_2n)))
+    #pooled_1D_2n = Flatten()(pooled_1D_2n)
+    #pooled_1D_2n = MLP_pooled_1D_2n(pooled_1D_2n)
 
-    pooled_1D_3n = maxPool2D_2x2(expand_dims(concat_conv1D_3n))
-    pooled_1D_3n = Flatten()(pooled_1D_3n)
-    pooled_1D_3n = MLP_pooled_1D_3n(pooled_1D_3n)
+    pooled_1D_3n = Flatten()(maxPool2D_2x2(expand_dims(concat_conv1D_3n)))
+    #pooled_1D_3n = Flatten()(pooled_1D_3n)
+    #pooled_1D_3n = MLP_pooled_1D_3n(pooled_1D_3n)
 
-    pooled_2D_2x2 = maxPool2D_2x2(concat_2D_2x2)
-    pooled_2D_2x2 = Flatten()(pooled_2D_2x2)
-    pooled_2D_2x2 = MLP_pooled_2D_2x2(pooled_2D_2x2)
+    pooled_2D_2x2 = Flatten()(maxPool2D_2x2(concat_2D_2x2))
+    #pooled_2D_2x2 = Flatten()(pooled_2D_2x2)
+    #pooled_2D_2x2 = MLP_pooled_2D_2x2(pooled_2D_2x2)
 
-    pooled_2D_3x3 = maxPool2D_2x2(concat_2D_3x3)
-    pooled_2D_3x3 = Flatten()(pooled_2D_3x3)
-    pooled_2D_3x3 = MLP_pooled_2D_3x3(pooled_2D_3x3)
+    pooled_2D_3x3 = Flatten()(maxPool2D_2x2(concat_2D_3x3))
+    #pooled_2D_3x3 = Flatten()(pooled_2D_3x3)
+    #pooled_2D_3x3 = MLP_pooled_2D_3x3(pooled_2D_3x3)
 
-    pooled_lstms = averagePool2D_2x2(expand_dims(concat_bilstms))
-    pooled_lstms = Flatten()(pooled_lstms)
-    pooled_lstms = MLP_pooled_lstms(pooled_lstms)
+    pooled_lstms = Flatten()(averagePool2D_2x2(expand_dims(concat_bilstms)))
+    #pooled_lstms = Flatten()(pooled_lstms)
+    #pooled_lstms = MLP_pooled_lstms(pooled_lstms)
 
-    out =  Lambda(lambda x: K.stack(x, axis=-1))([pooled_1D_2n, pooled_1D_3n, pooled_2D_2x2, pooled_2D_3x3,
+    out =  Lambda(lambda x: K.concatenate(x, axis=-1))([pooled_1D_2n, pooled_1D_3n, pooled_2D_2x2, pooled_2D_3x3,
                                                    pooled_lstms])
-    out = Flatten()(out)
+    #out = Flatten()(out)
     out = MLP_score(out)
 
     model = Model(inputs=[embedded_sentence_A, embedded_sentence_B], outputs=out)
@@ -198,15 +209,15 @@ network.summary()
 model_head = 'model_semanticsimilaritynetwork'
 time = time.strftime("%Y%m%d%H%M%S")
 dataset_size = 5700
-test_size = 1300
+test_size = 100
 epochs = 50
-model_name = model_head + '/' + configurations.EMBEDDER+str(EMBEDDING_LENGTH) + '_ep=' + str(epochs)+  '_act=' + selected_activation + '_slen=' + str(configurations.MAX_TEXT_WORD_LENGTH) \
+model_name = model_head + '/' + configurations.EMBEDDER+str(EMBEDDING_LENGTH) + '_bsize=' + str(BATCH_SIZE) +'_ep=' + str(epochs)+  '_act=' + selected_activation + '_slen=' + str(configurations.MAX_TEXT_WORD_LENGTH) \
              + '_dsize=' + str(dataset_size)+ '_tsize=' + str(test_size)+ '_' + time + '.h5'
 
 sentences_A, sentences_B, scores = read_sts_data('test')
 sentences_A = np.array([get_ready_vector(sentence) for sentence in sentences_A[0:test_size]])
 sentences_B = np.array([get_ready_vector(sentence) for sentence in sentences_B[0:test_size]])
-scores = minmax_scale(scores, feature_range=(0, 0.99))
+#scores = minmax_scale(scores, feature_range=(0, 0.99))
 scores = scores[0:test_size]
 
 val_data = [[sentences_A, sentences_B], scores]
@@ -218,8 +229,8 @@ if TRAIN:
     reduce_lr = ReduceLROnPlateau(monitor='loss', patience=5, factor=0.1, verbose=1, min_lr=0.0001)
     #checkpoint_callback = ModelCheckpoint(
         #filepath='trained_models/model_independent_2_02_RegularRNN_Fasttext_mixedmargin_update00.h5', period=1)
-    os.mkdir('./logs/'+ model_head + '/' + model_name)
-    tensorboard = TensorBoard(log_dir='./logs/'+ model_head + '/' + model_name,
+    os.mkdir('./logs/'+  model_name)
+    tensorboard = TensorBoard(log_dir='./logs/' + model_name,
                               histogram_freq=0,
                               batch_size=BATCH_SIZE,
                               write_graph=True, write_grads=False, write_images=False, embeddings_freq=0,
@@ -229,7 +240,8 @@ if TRAIN:
     K.get_session().run(tf.local_variables_initializer())
     network.fit_generator(generator=data_generator,validation_data=val_data,shuffle=True, epochs=epochs, workers=1, use_multiprocessing=False,
                         callbacks=[reduce_lr, tensorboard])
-    os.mkdir('trained_models/' + model_head)
+    if not os.path.exists('trained_models/' + model_head):
+        os.mkdir('trained_models/' + model_head)
     network.save('./trained_models/' + model_name + '.h5')
 else:
     #network = load_model('trained_models\model_independent_2_02_RegularRNN_Fasttext_mixedmargin.h5')
